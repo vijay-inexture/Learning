0b977088b278bc11cf7f4e26c1fec2598292385b
vijay-inexture <vijaydaraji@inexture.com>
2023-04-04 17:28:56 +0530

    init

---
+++ src/main/java/com/vijay/stackTask/Stack.java
@@ -0,0 +1,118 @@
+package com.vijay.stackTask;
+import java.util.Scanner;
+
+public class Stack {
+	
+	private int arr[];
+	private int capacity;
+	private int top;
+
+	public Stack() {
+		super();
+	}
+	
+	public void setStack(int size) {
+		this.arr = new int[size];
+		this.capacity = size;
+		this.top = -1;
+	}
+
+	public boolean isFull() {
+		if(top == capacity-1) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isEmpty() {
+		if(top == -1) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void push(int value) {
+		if(isFull()) {
+			System.out.println("Stack overflow, value not inserted");
+		}else {
+			arr[++top] = value;
+			System.out.println("Value push: "+value);
+		}
+	}
+	
+	public void pop() {
+		if(isEmpty()) {
+			System.out.println("Stack Empty");
+		}
+		else {
+			int popValue = arr[top--];
+			System.out.println("value pop:"+popValue);
+		}
+		
+	}
+	
+	public void display() {
+		if(isEmpty()) {
+			System.out.println("Stack is empty!");
+		}else {
+			for(int i=0;i<=top;i++) {
+				System.out.print(arr[i]+" ");
+			}
+		}
+	}
+	
+	public void pop(int pos) {
+		if(pos>top) {
+			System.out.println("Element not Exist at possition:"+pos);
+		}else {
+			int popEl = arr[pos];
+			while(pos<top) {
+				arr[pos] = arr[pos+1];
+				pos++;
+			}
+			top--;
+			System.out.println("value pop:"+popEl);
+		}
+	}
+	
+	public void push(int pushValue, int pos) {
+		Scanner scanner = new Scanner(System.in);
+		//stack is not full, user want to push to next top position
+		if(!isFull() && pos==top+1) {
+			arr[++top] = pushValue;
+			System.out.println("value push:"+pushValue+" at position: "+pos);
+			return;
+		}
+		//user enter position out of range
+		else if(pos>top) {
+			System.out.println("Invalid position!");
+			return;
+		}
+		//stack is full we need to replace element if user want
+		else if(isFull()) {
+			System.out.println("You want to replace Element? (y/n)");
+			String choice = scanner.nextLine();
+			if(choice.equals("y") || choice.equals("Y")) {
+				arr[pos] = pushValue;
+				System.out.println("value push:"+pushValue+" at position: "+pos);
+			}
+			else {
+				System.out.println("Value not pushed!");
+			}
+			return;
+		}
+		//stack is not full and user want to add between some element
+		else {
+			int loc = top;
+			while(pos<=loc) {
+				arr[loc+1] = arr[loc];
+				loc--;
+			}
+			arr[++loc] = pushValue;
+			++top;
+			System.out.println("value push:"+pushValue+" at position: "+pos);
+		}
+		
+	}
+
+}
---
+++ src/main/java/com/vijay/stackTask/StackTest.java
@@ -0,0 +1,120 @@
+package com.vijay.stackTask;
+
+import java.util.Scanner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StackTest {
+
+	public static void main(String args[]) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Stack stack = context.getBean("stackBean", Stack.class);  
+		
+		Scanner sc = new Scanner(System.in);
+		boolean flag = true;
+		String userChoice;
+		int choice = 0;
+		
+		int size;
+		boolean invalidInput = false;
+		do {
+			System.out.println("\nEnter Stack size: ");
+			String input = sc.nextLine();
+			
+			try {
+			    size = Integer.parseInt(input);
+			    stack.setStack(size);
+			    invalidInput = false;
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid Input!, Please Enter Valid Size.");
+				invalidInput = true;
+			}
+			
+		}while(invalidInput);
+		
+		while(flag) {
+			
+			System.out.println("\n1. for push");
+			System.out.println("2. for pop");
+			System.out.println("3. for Display");
+			System.out.println("4. for push from specific position");
+			System.out.println("5. for pop from specific position");
+			System.out.println("6. for exit");
+			
+			System.out.println("Please enter youe choice: ");
+			userChoice = sc.nextLine();
+			
+			if(isNumber(userChoice)) {
+				choice = Integer.parseInt(userChoice);
+			    invalidInput = false;
+			}else {
+				System.out.println("Invalid Input!, Please Enter Valid choice.");
+				invalidInput = true;
+			}
+			
+			if(!invalidInput) {
+				switch(choice) {
+												
+					case 1: 
+							System.out.println("enter value to push");
+							String pushValueStr = sc.nextLine();
+							if(isNumber(pushValueStr)) {
+								int pushValue = Integer.parseInt(pushValueStr);
+								stack.push(pushValue);
+							}else {
+								System.out.println("Invalid Input!, Please Enter Valid Value.");
+							}
+							break;
+							
+					case 2: 
+							stack.pop();
+							break;
+							
+					case 3: 
+							stack.display();
+							break;
+							
+					case 4: 
+							System.out.println("enter value to push");
+							String pushValueStr2 = sc.nextLine();
+							System.out.println("enter position for push");
+							String indexStr = sc.nextLine();
+							
+							if(isNumber(pushValueStr2) && isNumber(indexStr)) {
+								int pushValue2 = Integer.parseInt(pushValueStr2);
+								int index = Integer.parseInt(indexStr);
+								stack.push(pushValue2, index);;
+							}else {
+								System.out.println("Invalid Input!, Please Enter Valid Value.");
+							}
+							break;
+							
+					case 5: System.out.println("enter position for pop");
+							int pos = sc.nextInt();
+							stack.pop(pos);
+							break;
+						
+					case 6: 
+							flag = false;
+							System.out.println("Exit from system");
+							break;
+					
+					default: System.out.println("please enter valid choice");
+							break;
+					
+				}
+			}
+		}
+	}
+	
+	public static boolean isNumber(String input) {
+		try {
+		    Integer.parseInt(input);
+		    return true;
+		} catch (NumberFormatException e) {
+		    return false;
+		}
+	}
+	
+	
+}
