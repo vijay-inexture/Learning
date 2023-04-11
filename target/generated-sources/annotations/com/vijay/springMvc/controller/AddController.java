bba8ee9af3def7e6a0e8300888e0a305b56a7b1e
vijay-inexture <vijaydaraji@inexture.com>
2023-04-11 12:44:21 +0530

    RequestParam

--- target/generated-sources/annotations/com/vijay/springMvc/controller/AddController.java
+++ target/generated-sources/annotations/com/vijay/springMvc/controller/AddController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.vijay.springMvc.service.AddService;
@@ -13,10 +14,7 @@ import com.vijay.springMvc.service.AddService;
 public class AddController {
 	
 	@RequestMapping("/add")
-	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
-		int i = Integer.parseInt(request.getParameter("t1"));
-		int j = Integer.parseInt(request.getParameter("t2"));
-		
+	public ModelAndView add(@RequestParam("t1") int i,@RequestParam("t2") int j) {		
 		AddService as = new AddService();
 		int k = as.add(i, j);
 		ModelAndView mv = new ModelAndView();
